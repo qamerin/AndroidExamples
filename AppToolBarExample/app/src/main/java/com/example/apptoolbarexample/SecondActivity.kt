27f7de8d0f85a2131d1503e7e1767ed2cbb5eeb4
Hiroki Kameyama <hkameyam@gmail.com>
2024-08-10 15:03:49 +0900

    add transition to second page

--- AppToolBarExample/app/src/main/java/com/example/apptoolbarexample/SecondActivity.kt
+++ AppToolBarExample/app/src/main/java/com/example/apptoolbarexample/SecondActivity.kt
@@ -1,27 +1,24 @@
 package com.example.apptoolbarexample
 
-import android.content.Intent
+import android.R
 import android.os.Bundle
-import android.widget.Button
+import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        setContentView(com.example.apptoolbarexample.R.layout.activity_second)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(com.example.apptoolbarexample.R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val btnSecond:Button = findViewById(R.id.btSecondPage)
-        btnSecond.setOnClickListener {
-            val intent = Intent(this,SecondActivity::class.java)
-            startActivity(intent)
         }
     }
 }
