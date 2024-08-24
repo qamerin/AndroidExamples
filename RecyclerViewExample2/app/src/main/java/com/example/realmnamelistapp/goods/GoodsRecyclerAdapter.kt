2f3ae1f065ba5eb37d89eb4cec4b41b109edc5f5
Hiroki Kameyama <hkameyam@gmail.com>
2024-08-24 17:37:41 +0900

    modify category

--- RecyclerViewExample2/app/src/main/java/com/example/realmnamelistapp/goods/GoodsRecyclerAdapter.kt
+++ RecyclerViewExample2/app/src/main/java/com/example/realmnamelistapp/goods/GoodsRecyclerAdapter.kt
@@ -6,26 +6,35 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.realmnamelistapp.R
 import com.example.realmnamelistapp.ViewHolderItem
+import com.example.realmnamelistapp.model.CategoryMasterModel
 import com.example.realmnamelistapp.model.GoodsModel
+import io.realm.Realm
 import io.realm.RealmResults
+import io.realm.kotlin.where
 
-class GoodsRecyclerAdapter(realmResults:RealmResults<GoodsModel>):RecyclerView.Adapter<ViewHolderItem>() {
+class GoodsRecyclerAdapter(realmResults:RealmResults<GoodsModel>):RecyclerView.Adapter<GoodsViewHolderItem>() {
+    private lateinit var realm: Realm
     private val rResults:RealmResults<GoodsModel> = realmResults
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderItem {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GoodsViewHolderItem {
         val oneXml = LayoutInflater.from(parent.context)
             .inflate(R.layout.goods_layout,parent,false)
-        return ViewHolderItem(oneXml)
+        return GoodsViewHolderItem(oneXml)
     }
 
     override fun getItemCount(): Int {
         return rResults.size
     }
 
-    override fun onBindViewHolder(holder: ViewHolderItem, position: Int) {
+    override fun onBindViewHolder(holder: GoodsViewHolderItem, position: Int) {
         val myModel = rResults[position]
         holder.oneTvName.text = myModel?.name.toString()
-        holder.oneTvAge.text = myModel?.category.toString()
+
+        // get Category Name
+        realm = Realm.getDefaultInstance()
+        val categoryMasterModelResult = realm.where<CategoryMasterModel>()
+            .equalTo("categoryId",myModel?.categoryId).findFirst()
+        holder.oneTvCategory.text = categoryMasterModelResult?.categoryName
 
         holder.itemView.setOnClickListener {
             val intent = Intent(it.context, GoodsAddActivity::class.java)
--- RecyclerViewExample2/app/src/main/java/com/example/realmnamelistapp/goods/GoodsViewHolderItem.kt
+++ RecyclerViewExample2/app/src/main/java/com/example/realmnamelistapp/goods/GoodsViewHolderItem.kt
@@ -7,6 +7,6 @@ import com.example.realmnamelistapp.R
 
 class GoodsViewHolderItem(v:View) :RecyclerView.ViewHolder(v){
     var oneTvName: TextView = v.findViewById(R.id.oneTvName)
-    var oneTvAge: TextView = v.findViewById(R.id.oneTvAge)
+    var oneTvCategory: TextView = v.findViewById(R.id.oneTvCategory)
 
 }
